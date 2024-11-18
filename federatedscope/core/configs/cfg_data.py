b55cd10b6aca6e964e1fde8eba5aecab25975cce
Jordi Luque <jordi.luque@telefonica.com>
2024-11-18 14:03:36 +0000

    Adding some configurations for asr

--- federatedscope/core/configs/cfg_data.py
+++ federatedscope/core/configs/cfg_data.py
@@ -14,6 +14,9 @@ def extend_data_cfg(cfg):
 
     cfg.data.root = 'data'
     cfg.data.type = 'toy'
+    cfg.data.language = 'english'
+    cfg.data.language_abbr = 'en'
+    cfg.data.task = 'transcribe'
     cfg.data.save_data = False  # whether to save the generated toy data
     cfg.data.args = []  # args for external dataset, eg. [{'download': True}]
     cfg.data.splitter = ''
