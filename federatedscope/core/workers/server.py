8562104eee35b05a0932b345bd6f122e1fff6857
Aleix Sant <aleix.santsavall@telefonica.com>
2025-01-27 13:05:56 +0000

    Prints the loss values for the servers validation and test sets, as well as for each client's validation and test sets, including the mean of each set across all clients, when make_global_eval=False

--- federatedscope/core/workers/server.py
+++ federatedscope/core/workers/server.py
@@ -602,7 +602,7 @@ class Server(BaseServer):
                 formatted_logs = self._monitor.format_eval_res(
                     metrics_all_clients,
                     rnd=round,
-                    role='Server #',
+                    role='Server # (mean)',
                     forms=self._cfg.eval.report)
                 if merge_type == "unseen":
                     for key, val in copy.deepcopy(formatted_logs).items():
@@ -931,7 +931,6 @@ class Server(BaseServer):
         To conduct evaluation. When ``cfg.federate.make_global_eval=True``, \
         a global evaluation is conducted by the server.
         """
-
         if self._cfg.federate.make_global_eval:
             # By default, the evaluation is conducted one-by-one for all
             # internal models;
@@ -960,7 +959,29 @@ class Server(BaseServer):
                 logger.info(formatted_eval_res)
             self.check_and_save()
         else:
-            # Preform evaluation in clients
+            for i in range(self.model_num):
+                trainer = self.trainers[i]
+                # Perform evaluation in server
+                metrics = {}
+                for split in self._cfg.eval.split: 
+                    eval_metrics = trainer.evaluate(
+                        target_data_split_name=split)
+                    metrics.update(**eval_metrics)
+                formatted_eval_res = self._monitor.format_eval_res(
+                    metrics,
+                    rnd=self.state,
+                    role='Server #',
+                    forms=self._cfg.eval.report,
+                    return_raw=True)
+                self._monitor.update_best_result(
+                    self.best_results,
+                    formatted_eval_res['Results_raw'],
+                    results_type="server_global_eval")
+                self.history_results = merge_dict_of_results(
+                    self.history_results, formatted_eval_res)
+                self._monitor.save_formatted_results(formatted_eval_res)
+                logger.info(formatted_eval_res) 
+            # Perform evaluation in clients
             self.broadcast_model_para(msg_type='evaluate',
                                       filter_unseen_clients=False)
 
